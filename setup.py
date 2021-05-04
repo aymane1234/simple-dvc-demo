27c36452d59b1f134fa7d19d54e0049f150d8e60
aymane <aymane1000@live.fr>
2021-05-04 11:57:10 +0200

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version = "0.0.1",
+    description = "This is Wine Quality package",
+    author = "the amazing Aymane",
+    package =find_packages(),
+    licence = "MIT",
+)
