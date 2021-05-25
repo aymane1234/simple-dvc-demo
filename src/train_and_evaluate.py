f05e7c4822354ce224ba83fb0aa3ac5a88e6873e
aymane <aymane1000@live.fr>
2021-05-25 22:52:30 +0200

    Webapp structure added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -71,7 +71,10 @@ def train_and_evaluate(config_path):
             "l1_ratio": l1_ratio,
         }
         json.dump(params, f, indent=4)
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
 
+    joblib.dump(lr, model_path)
 
 
 if __name__ == "__main__":
